c935d0952e21a300d6be168117ad8b2b5f119131
Fr4nco77 <fcarreras777@gmail.com>
2025-09-09 12:36:31 -0300

    Add footer and navigation components with internationalization support

--- src/i18n/ui/en.ts
+++ src/i18n/ui/en.ts
@@ -1,3 +1,10 @@
 export default {
-    
-}
+  nav: {
+    catalog: "Catalog",
+    blog: "Blog",
+    contact: "Contact",
+  },
+  footer: {
+    copyright: "®Copyrigth 2025 PH. All rights reserved"
+  },
+};
--- src/i18n/ui/es.ts
+++ src/i18n/ui/es.ts
@@ -1,3 +1,10 @@
 export default {
-    
-}
+  nav: {
+    catalog: "Catálogo",
+    blog: "Blog",
+    contact: "Contacto",
+  },
+  footer: {
+    copyright: "®Copyright 2025 PH. Todos los derechos reservados.",
+  },
+};
