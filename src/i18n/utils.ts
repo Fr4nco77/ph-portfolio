840c60819e298e926e0dd46506e44497ae42a232
Franco Carreras <fcarreras777@gmail.com>
2025-10-04 01:42:13 -0300

    Remove unused localPaths function from utils.ts

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -2,13 +2,3 @@ export const locales = {
   en: "EN",
   es: "ES",
 } as const;
-
-export function localPaths() {
-  return Object.keys(locales).map((key) => {
-    return {
-      params: {
-        lang: key,
-      },
-    };
-  });
-}
