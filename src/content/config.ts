5fe65acb414b66625d9bf422c7e64da0c084378e
Franco Carreras <fcarreras777@gmail.com>
2025-10-04 23:53:09 -0300

    add blog collectioon

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,9 +12,19 @@ const projectCollection = defineCollection({
     }),
 });
 
-// const blogCollection = defineCollection({});
+const blogCollection = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/blogs" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().min(5),
+      description: z.string().min(10),
+      cover: image(),
+      date: z.date(),
+      tag: z.string().min(2),
+    }),
+});
 
 export const collections = {
   projects: projectCollection,
-  //   'blog': blogCollection,
+  blogs: blogCollection,
 };
